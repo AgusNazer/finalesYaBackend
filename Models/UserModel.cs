30da8f5b0c69f0fe292938199c654209a476f8c8
agusNazer <agustin.nazer@hotmail.com>
2025-07-09 23:49:43 -0300

    first commit

---
+++ Models/UserModel.cs
@@ -0,0 +1,23 @@
+namespace finalesYaBackend.Models
+
+{
+    public enum UserRole
+    {
+        User,
+        SuperUser
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string? University { get; set; }
+        public UserRole Role { get; set; } = UserRole.User;
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        public List<Subject> Subjects { get; set; } = new();
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
