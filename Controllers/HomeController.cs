6a8c542aae512b007fc1e52cedd87c3b9efccccf
agusNazer <agustin.nazer@hotmail.com>
2025-07-12 00:34:40 -0300

    feat: added swagger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace finalesYaBackend.Controllers;
 
 public class HomeController : Controller
 {
+    
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
