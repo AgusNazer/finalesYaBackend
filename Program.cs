af438d98048d1b97c7dfa36614cf7b5b1e7687f2
agusNazer <agustin.nazer@hotmail.com>
2025-08-08 01:23:24 -0300

    feat: added cor nelify

--- Program.cs
+++ Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://finalesyafrontend.netlify.app")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
